46dc7d11865b57ebcbd078df1b444bd37459f57d
Kalpesh_Nimje <kalpesh22nimje@gmail.com>
2024-06-14 16:46:37 +0530

    chore: Update tasks.json to use g++ for compilation and fix output file path

---
+++ leetcode/palindromeNumber.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        if (x < 0)
+        {
+            return false;
+        }
+
+        long long int reversed = 0;
+        long long int num = x;
+        while (num != 0)
+        {
+            int digit;
+            digit = num % 10;
+            reversed = digit + (reversed * 10);
+            num /= 10;
+        }
+        return (reversed == x);
+    }
+};
+
+int main()
+{
+    Solution sol;
+    int x;
+    cin >> x;
+    cout << sol.isPalindrome(x) << endl;
+    return 0;
+}
