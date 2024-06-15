f0cf04f001ea4e659783b362317760afe87e884d
Kalpesh_Nimje <kalpesh22nimje@gmail.com>
2024-06-15 23:51:37 +0530

    More Problems added

---
+++ leetcode/plusOne.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+        bool nine = true;
+
+        for (int i = digits.size() - 1; i >= 0; i--)
+        {
+            if (digits[i] == 9 && nine)
+                digits[i] = 0;
+            else
+            {
+                if (nine)
+                    digits[i] += 1;
+                nine = false;
+            }
+        }
+        if (nine)
+        {
+            digits.emplace(digits.begin(), 1);
+        }
+        return digits;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> digits;
+    for(int i = 0; i < 3; i++)
+    {
+        int x;
+        cin >> x;
+        digits.push_back(x);
+    }
+    vector<int> result = s.plusOne(digits);
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
